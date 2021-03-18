ff1bffb1143ee145c2793a366b5ebe103ea90fdf
Ítalo Martins <italo.mtr@gmail.com>
2021-03-18 18:03:12 -0300

    para as girls verem

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ import usersRouters from './routes/users.js';
 // The whole application lies on this funtion
 const app = express(); 
 const PORT = process.env.PORT || 3000;
-console.log('funcionou alal');
+
 
 app.use(bodyParser.json());
 
 app.use('/users', usersRouters);
 
 
-app.get('/',(req,res)=>{ res.send('Hello from Homepage');});
+app.get('/',(req,res)=>{ res.send('Oi girlss eu consegui subir essa bagaça !!!!!');});
 
 app.listen(PORT,() => console.log('server Running on port: http://localhost:' + PORT));
