d05c0cd537ae9df598b187796d3ce5538e9cb410
patrcastro <patrcastro@hotmail.com>
2021-03-21 15:52:15 -0300

    Conexão com o MongoDB e Cadastro de Professores

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import {
+  getUsers,
+  createUser
+} from '../controllers/userController'
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.get('/usuarios', (req, res) => {
+  getUsers(req, res)
+})
+
+app.post('/usuario', (req, res) => {
+  console.log('chegou')
+  console.log(req.body)
+  createUser(req.body.login, req.body.password, req.body.nome, res)
+})
+export default app
